2ee6f896ebe95f5baecd2ade6fd58ab9d8023e03
sumangalasipoy@gmail.com <sumangalasipoy@gmail.com>
2026-01-29 12:49:04 +0700

    Fix: add @vitejs/plugin-react for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // For a user site hosted at https://<username>.github.io/, use root base
-  base: "/",
 })
